038bcc62a023b94e44934fe90b50e61b7d1158d8
Travis Kohlbeck <tdkohlbeck+github@gmail.com>
2017-02-04 18:51:31 -0800

    lesson 23: extracting container component

--- getting-started/src/App.js
+++ getting-started/src/App.js
@@ -1,10 +1,5 @@
-// @flow
-
 import React, { Component } from 'react';
-import {
-  createStore,
-  combineReducers,
-} from 'redux';
+import { createStore, combineReducers } from 'redux';
 
 import './App.css';
 
@@ -57,89 +52,11 @@ const todoApp = combineReducers({
   visFilter,
 });
 
-// same as:
-//
-//const todoApp = (state = {}, action) => {
-//  return {
-//    todos: todos(state.todos, action),
-//    visFilter: visFilter(state.visFilter, action),
-//  };
-//};
-
 const store = createStore(todoApp);
-
-const FilterLink = ({
-  currFilter,
-  filter,
-  children,
-  onClick,
-}) =>  { // href='#' -> e.preventDefault() ?
-  if (filter === currFilter) {
-    return (<span>{children}</span>);
-  }
-  return (
-    <a
-      href='#'
-      onClick={e => {
-        e.preventDefault();
-        onClick(filter);
-      }}
-    >
-      {children}
-    </a>
-  );
-};
-
-const getVisTodos = (
-  todos,
-  filter
-) => {
-  switch (filter) {
-    case 'SHOW_ALL': return todos;
-    case 'SHOW_COMPLETED': return todos.filter(
-      t => t.completed
-    );
-    case 'SHOW_ACTIVE': return todos.filter(
-      t => !t.completed
-    );
-    default: return todos;
-  }
-};
-
-const Todo = ({
-  onClick,
-  completed,
-  text
-}) => (
-  <li
-    onClick={onClick}
-    style={{
-      textDecoration: completed ?
-        'line-through' :
-        'none'
-    }}>
-    {text}
-  </li>
-);
-
-const TodoList = ({
-  todos,
-  onTodoClick,
-}) => (
-  <ul>
-    {todos.map(todo =>
-      <Todo
-        {...todo}
-        onClick={() => onTodoClick(todo.id)}
-        key={todo.id}
-      />
-    )}
-  </ul>
-);
 
 const AddTodo = ({
   onAddClick,
-}) => {
+  }) => {
   let input;
   return (
     <div>
@@ -156,30 +73,118 @@ const AddTodo = ({
   );
 };
 
-const Footer = ({
-  visFilter,
-  onFilterClick,
-}) => (
+const Todo = ({
+  onClick,
+  completed,
+  text
+  }) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration: completed ?
+        'line-through' :
+        'none'
+    }}>
+    {text}
+  </li>
+);
+
+const getVisTodos = (
+  todos,
+  filter
+  ) => {
+  switch (filter) {
+    case 'SHOW_ALL': return todos;
+    case 'SHOW_COMPLETED': return todos.filter(
+      t => t.completed
+    );
+    case 'SHOW_ACTIVE': return todos.filter(
+      t => !t.completed
+    );
+    default: return todos;
+  }
+};
+
+const TodoList = ({
+  todos,
+  onTodoClick,
+  }) => (
+  <ul>
+    {todos.map(todo =>
+      <Todo
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}
+        key={todo.id}
+      />
+    )}
+  </ul>
+);
+
+const Link = ({
+  active,
+  children,
+  onClick,
+  }) =>  {
+  if (active) {
+    return (<span>{children}</span>);
+  }
+  return (
+    <a
+      href='#'
+      onClick={e => {
+        e.preventDefault();
+        onClick();
+      }}
+    >
+      {children}
+    </a>
+  );
+};
+
+class FilterLink extends Component {
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() =>
+      this.forceUpdate()
+    );
+  }
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+  render() {
+    const props = this.props;
+    const state = store.getState();
+
+    return (
+      <Link
+        active={props.filter === state.visFilter}
+        onClick={() =>
+          store.dispatch({
+            type: 'SET_VIS_FILTER',
+            filter: props.filter,
+          })
+        }
+      >
+        {props.children}
+      </Link>
+    );
+  }
+}
+
+const Footer = () => (
   <p>
     Show:
     {' '}
     <FilterLink
       filter='SHOW_ALL'
-      currFilter={visFilter}
-      onClick={onFilterClick}
-    > All </FilterLink>
-    {' '}
+    >All</FilterLink>
+    {', '}
     <FilterLink
       filter='SHOW_ACTIVE'
-      currFilter={visFilter}
-      onClick={onFilterClick}
-    > Active </FilterLink>
-    {' '}
+    >Active</FilterLink>
+    {', '}
     <FilterLink
       filter='SHOW_COMPLETED'
-      currFilter={visFilter}
-      onClick={onFilterClick}
-    > Completed </FilterLink>
+    >Completed</FilterLink>
   </p>
 );
 
@@ -187,7 +192,7 @@ let nextTodoId = 0;
 const TodoApp = ({
   todos,
   visFilter,
-}) => (
+  }) => (
   <div>
 
     <AddTodo
@@ -199,7 +204,6 @@ const TodoApp = ({
         })
       }
     />
-
     <TodoList
       todos={getVisTodos(
         todos,
@@ -212,17 +216,7 @@ const TodoApp = ({
         })
       }
     />
-
-    <Footer
-      visFilter={visFilter}
-      onFilterClick={filter =>
-        store.dispatch({
-          type: 'SET_VIS_FILTER',
-          filter,
-        })
-      }
-    />
-
+    <Footer />
   </div>
 );
 
